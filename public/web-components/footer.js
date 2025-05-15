bfeeeb16207692f474c47e05c0afcbcf05b02bc8
ffilloux <fillouxflorian56@mail.com>
2025-05-15 15:17:53 +0200

    feat : responsif et plus d'infos sur mon parcours

--- public/web-components/footer.js
+++ public/web-components/footer.js
@@ -3,7 +3,7 @@ class SiteFooter extends HTMLElement {
     this.innerHTML = `
       <footer class="layout__footer">
         <img class="layout__footer-logo" src="./assets/imgs/logo-icon.png" alt="Logo du site représentant une fusée" />
-        <nav class="layout__nav">
+        <nav class="layout__nav2">
           <a href="index.html">Accueil</a>
           <a href="#top">Aller en haut</a>
         </nav>
@@ -12,7 +12,13 @@ class SiteFooter extends HTMLElement {
 
     const style = document.createElement('style');
     style.textContent = `
-      .layout__nav a {
+      .layout__nav2 {
+        display: flex;
+        flex-direction : row;
+        gap: 1rem;
+      }
+
+      .layout__nav2 a {
         color: var(--primary-text-color);
         text-decoration: none;
         font-weight: 600;
